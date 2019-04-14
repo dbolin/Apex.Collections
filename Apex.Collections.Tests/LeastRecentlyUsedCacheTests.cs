e2a64faae3a27815a46f6b65c5ad0a55d5fa7c3e
dbolin <dbolin.m@gmail.com>
2019-04-14 18:49:55 -0400

    LRU cache implementation

---
+++ Apex.Collections.Tests/LeastRecentlyUsedCacheTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Apex.Collections.Tests
+{
+    public class LeastRecentlyUsedCacheTests
+    {
+
+        public static IEnumerable<object[]> Test1Data = new[] {
+            new [] {
+                new [] { "a", "bc", "def", "a", "ghij", "a", "klmno", "bc", "a", "p", "qr", "bc", "bc"}
+            }
+        };
+
+        [Theory]
+        [MemberData(nameof(Test1Data))]
+        public void Test1(string[] keys)
+        {
+            var sut = new LeastRecentlyUsedCache<string, int>(5);
+
+            AssertContentsPerStep(sut, k => k.Length, keys, new[] {
+                new [] { "a" },
+                new [] { "a", "bc" },
+                new [] { "a", "bc", "def" },
+                new [] { "bc", "def", "a" },
+                new [] { "bc", "def", "a", "ghij" },
+                new [] { "bc", "def", "ghij", "a" },
+                new [] { "bc", "def", "ghij", "a", "klmno" },
+                new [] { "def", "ghij", "a", "klmno", "bc" },
+                new [] { "def", "ghij", "klmno", "bc", "a" },
+                new [] { "ghij", "klmno", "bc", "a", "p" },
+                new [] { "klmno", "bc", "a", "p", "qr" },
+                new [] { "klmno", "a", "p", "qr", "bc" },
+                new [] { "klmno", "a", "p", "qr", "bc" },
+                }
+            );
+        }
+
+        private void AssertContentsPerStep<K, V>(LeastRecentlyUsedCache<K, V> sut, Func<K, V> valueCreator, K[] keys, K[][] stepValues)
+        {
+            stepValues.Length.Should().Be(keys.Length, "one result is needed per step key");
+
+            for(int i=0;i<keys.Length;++i)
+            {
+                sut.GetOrAdd(keys[i], valueCreator);
+
+                AssertContents(sut, valueCreator, stepValues[i]);
+            }
+        }
+
+        private void AssertContents<K, V>(LeastRecentlyUsedCache<K, V> sut, Func<K, V> valueCreator, K[] k)
+        {
+            int index = 0;
+            foreach(var kvp in sut)
+            {
+                var expectedKey = k[index];
+                var expectedValue = valueCreator(expectedKey);
+
+                kvp.Should().BeEquivalentTo(new KeyValuePair<K, V>(expectedKey, expectedValue));
+                index++;
+            }
+        }
+    }
+}
---
+++ Apex.Collections/LeastRecentlyUsedCache.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Apex.Collections
+{
+    public sealed class LeastRecentlyUsedCache<K, V> : IEnumerable<KeyValuePair<K, V>>
+    {
+        private readonly Dictionary<K, LinkedListNode<KeyValuePair<K, V>>> _lookup;
+        private readonly LinkedList<KeyValuePair<K, V>> _list;
+
+        public int Capacity { get; }
+
+        public LeastRecentlyUsedCache(int capacity, IEqualityComparer<K> equalityComparer = null)
+        {
+            Capacity = capacity;
+            _lookup = new Dictionary<K, LinkedListNode<KeyValuePair<K, V>>>(capacity, equalityComparer ?? EqualityComparer<K>.Default);
+            _list = new LinkedList<KeyValuePair<K, V>>();
+        }
+
+        public V GetOrAdd(K key, Func<K, V> valueCreator)
+        {
+            V result;
+            if(Get(key, out result))
+            {
+                return result;
+            }
+
+            if(_list.Count >= Capacity)
+            {
+                RemoveFirst();
+            }
+
+            result = valueCreator(key);
+            Add(key, result);
+
+            return result;
+        }
+
+        private void RemoveFirst()
+        {
+            var lru = _list.First;
+            _lookup.Remove(lru.Value.Key);
+            _list.RemoveFirst();
+        }
+
+        private void Add(K key, V value)
+        {
+            var newNode = _list.AddLast(new KeyValuePair<K, V>(key, value));
+            _lookup.Add(key, newNode);
+        }
+
+        private bool Get(K key, out V value)
+        {
+            if (_lookup.TryGetValue(key, out var node))
+            {
+                _list.Remove(node.Value);
+                _list.AddLast(node.Value);
+                value = node.Value.Value;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator() => _list.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => _list.GetEnumerator();
+    }
+}
